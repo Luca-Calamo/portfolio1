30b6f2222df22b5d55515eb7a3e184a3de9c2601
luca_calamo <lucacalamo13@gmail.com>
2026-01-21 11:35:12 -0800

    added under construction paragraph

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,6 +8,7 @@ export default function Home() {
             <section className={styles.hero}>
                 <h1 className={styles.title}>Hey there, I'm Luca!</h1>
                 <p className={styles.subtitle}>Digital Marketer / Designer</p>
+                <p className={styles.under}>This site is currently being built! <br /> If you have any questions about my work, please feel free to reach out to me at <a href="mailto:lucacalamo13@gmail.com">lucacalamo13@gmail.com</a></p>
             </section>
 
             <section className={styles.featured}>
