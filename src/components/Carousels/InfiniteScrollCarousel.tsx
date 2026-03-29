66ae1e1016e2706257006b0ab475c959995b9a1f
luca_calamo <lucacalamo13@gmail.com>
2026-03-29 16:48:53 -0700

    fixed deployment error

--- src/components/Carousels/InfiniteScrollCarousel.tsx
+++ src/components/Carousels/InfiniteScrollCarousel.tsx
@@ -95,7 +95,6 @@ export default function InfiniteScrollCarousel({
                                         autoPlay
                                         muted
                                         loop
-                                        loading='lazy'
                                     />
                                 ) : (
                                     <img
